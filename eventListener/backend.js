5041f06a112c70b7d957d0b796eef923333ff11b
Vergin <mrtemnmie@gmail.com>
2025-05-20 15:12:07 +0200

    commit that shit dangit

--- eventListener/backend.js
+++ eventListener/backend.js
@@ -12,7 +12,24 @@ for (let checkbox of checkboxElems) {
 
     if(this.name == "audi"){
         if(this.checked){
+             const audiDir = "./AUDI";
+                const gallery = document.getElementById('gallery');
+                const images = [
+                '1.webp',
+                '2.webp',
+                '3.webp',
+                '4.webp',
+                '5.webp',
+                '6.webp'
+                ];
 
+                images.forEach(image => {
+                const img = document.createElement('img');
+                img.src = `${audiDir}/${image}`;
+                img.alt = 'Gallery Image';
+                img.classList.add('gallery-img');
+                gallery.appendChild(img);
+                });
         } else {
 
         }
@@ -20,7 +37,10 @@ for (let checkbox of checkboxElems) {
 
     if(this.name == "mercedes"){
         if(this.checked){
-
+            const MercedesDir = "eventListener\\Mercedes";
+            if(MercedesDir.length){
+               console.log(MercedesDir.length);
+            }
         } else {
 
         }
@@ -28,7 +48,10 @@ for (let checkbox of checkboxElems) {
 
     if(this.name == "bmw"){
         if(this.checked){
-
+            const bmwDir = "eventListener\\BMW";
+            if(bmwDir.length){
+               console.log(bmwDir.length);
+            }
         } else {
 
         }
